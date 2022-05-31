2ed623ccc76587b578e0ba0ccc2495b83817da01
Lelahelry <lelahelry@live.fr>
2022-05-31 14:42:08 +0200

    ouais

--- game.py
+++ game.py
@@ -1,32 +1,55 @@
 import os
 import time
 import random
+import functions as f
 class ship:
+    """
+    Class which manages the shape and attributes of ships on the map.
+    """
     def __init__(self, name: str, shape: list[tuple[bool, ...]]):
+        """
+        Ship initialization, needs a name and shape
+        """
         self.name = name
         self.shape = shape
         self.sunk = False
         self.placed = False
 
     def __str__(self):
+        """
+        Returns string representation of the ship for text mode
+        """
         lines = [" __"*len(self.shape[0])] + ["|" + "".join(["██|" if cell else "__|" for cell in row]) for row in self.shape]
         return "\n".join(lines)
 
     def clockwise_rotate(self):
+        """
+        Rotates ship list representation to the right
+        """
         assert not self.placed, "Ship is anchored, cannot be rotated anymore!"
 
         self.shape = list(zip(*self.shape[::-1]))
 
     def anticlockwise_rotate(self):
+        """
+        Rotates ship list representation to the right.
+        Redundant with clockwise_rotate if ships are linear but useful if they are not.
+        """
         assert not self.placed, "Ship is anchored, cannot be rotated anymore!"
         
         self.shape = list(zip(*self.shape))[::-1]
     
     def place(self, positions: set[tuple[int, int]]):
+        """
+        Updates a ship as placed.
+        """
         self.positions = {cellpos: True for cellpos in positions}
         self.placed = True
 
     def damage(self, xtar: int, ytar: int):
+        """
+        Handles shooting a ship and returns the associated cell state. S for sunk and H for hit
+        """
         assert (ytar, xtar) in self.positions.keys(), "Position already hit."
 
         self.positions[(ytar, xtar)] = False
@@ -40,19 +63,33 @@ class ship:
 
 
 class player:
-    def __init__(self, name: str, gridsize: int, ships: list[ship], isAI: bool = False):
+    """
+    This class represents a full battleships player, with the associated actions it can take and its own battlefield.
+    """
+    def __init__(self, name: str, gridsize: int, ships: list[ship], isAI: bool=False):
+        """
+        Simple player constructor.
+        """
         self.name = name
         self.ships = ships
         self.isAI = isAI
         self.alive = True
         self.opponent = None
-        self.map = [["WU"] * gridsize for _ in range(gridsize)]
+        self.map = [["WU"] * gridsize for _ in range(gridsize)] # All cells start out like this, W means water and U means unhit.
         self.hits = [None]
+        self.graphical_parameters = None
 
     def set_opponent(self, opponent: 'player'):
+        """
+        Sets the player's opponent for upcoming game.
+        """
         self.opponent = opponent
     
-    def strmap(self, *, full: bool = False):
+    def strmap(self, *, full: bool=False):
+        """
+        Returns string representation of the player's battlefield for text mode.
+        Either full one for ship placement and endgame or hidden one which only shows known positions.
+        """
         lines = ["  " + ''.join([f" {j:2}" for j in range(len(self.map[0]))])]
         if full:
             lines += [f"{i} |" + ''.join(["__|" if cell[:-1] == 'W' else "()|" if cell[-1] == 'M' else "##|" if cell[-1] == "H" else "░░|" if cell[-1] == "S" else "██|" for cell in row]) for i, row in enumerate(self.map)]
@@ -61,7 +98,11 @@ class player:
         
         return "\n".join(lines)
     
-    def place_ship(self, xpos: int, ypos: int, ship: ship, occupied_positions: set[tuple[int, int]], *, verbose = True):
+    def place_ship(self, xpos: int, ypos: int, ship: ship, occupied_positions: set[tuple[int, int]], *, verbose: bool=True):
+        """
+        Method which validates ship placement, returns set containing its positions if valid, else returns empty set.
+        Is able to print warning messages or not, depending on where it is used.
+        """
         ship_positions = {(ypos + i, xpos + j) for i, row in enumerate(ship.shape) for j, cell in enumerate(row) if cell}
         
         if not occupied_positions.isdisjoint(ship_positions):
@@ -79,7 +120,11 @@ class player:
             
             return ship_positions
     
-    def target_check(self, xtar, ytar, *, verbose = True):
+    def target_check(self, xtar: int, ytar: int, *, verbose: bool=True):
+        """
+        Method which validates given targets for upcoming shot.
+        Is able to print warning messages or not, depending on where it is used.
+        """
         if not (0 <= xtar < len(self.map[0])) or not (0 <= ytar < len(self.map)):
             if verbose:
                 os.system("clear||cls")
@@ -94,37 +139,50 @@ class player:
             return True
     
     def setup(self, *, graphical: bool = False):
-        if self.isAI:
+        """
+        First big gameplay loop corresponding to setup phase.
+        Handles the full determination of a player's battlefield, be it in text mode or graphical mode, AI player or real one.
+        """
+        if self.isAI: # AI player branch.
             print(f"{self.name} is setting up its battlefield.")
-            occupied_positions = set()
+            occupied_positions = set() # Set of all occupied positions of the player's boats, starts empty.
             for idx, ship in enumerate(self.ships):
-                rotate_choice = bool(random.randint(0, 1))
+                rotate_choice = bool(random.randint(0, 1)) # Decision to rotate is randomly determined.
                 if rotate_choice:
                     ship.clockwise_rotate()
-
-                ship_positions = set()
+                
+                ship_positions = set() # Set of current ship placed positions, starts empty.
                 while ship_positions == set():
                     if graphical:
                         pass
-                        
+                    
+                    # AI chooses a random spot to place its ship, which may or may not be valid
+                    # It then tries to place its ship, and if it doesn't work it'll go through the same process again in the next loop iteration.
                     xanchor, yanchor = random.randint(0, len(self.map[0])), random.randint(0, len(self.map))
                     ship_positions = self.place_ship(xanchor, yanchor, ship, occupied_positions, verbose=False)
-
+                
+                # Updating the actual map with definitive positions of the ship.
+                # The ID of the ship is associated with unhit status in the map cell.
                 for ypos, xpos in ship_positions:
                     self.map[ypos][xpos] = str(idx) + 'U'
                 
+                # Finally, the ship object itself is updated.
+                # As is the set of occupied positions that is filled with the positions of the recently placed ship.
                 ship.place(ship_positions)
                 occupied_positions.update(ship_positions)
             
             print(f"\n{self.name} has placed its ships!\n")
             time.sleep(1.5)
-        else:
-            occupied_positions = set()
+        else: # Real player branch
+            occupied_positions = set() # Set of all occupied positions of the player's boats, starts empty.
             for idx, ship in enumerate(self.ships):
-                ship_positions = set()
+                ship_positions = set()  # Set of current ship placed positions, starts empty.
                 while ship_positions == set():
                     if graphical:
-                        pass
+                        xpos, ypos = f.get_graphical_pos(**self.graphical_parameters)
+                        
+                        
+                        ship_positions = self.place_ship(xpos, ypos, ship, occupied_positions)
                     else:
                         print(f"\n{self.name}, here's your battlefield's in its current state:\n")
                         print(self.strmap(full=True))
@@ -146,10 +204,14 @@ class player:
                         else:
                             os.system("clear||cls")
                             print("\nInvalid inputs, please try again.")
-
+                
+                # Updating the actual map with definitive positions of the ship.
+                # The ID of the ship is associated with unhit status in the map cell.
                 for ypos, xpos in ship_positions:
                     self.map[ypos][xpos] = str(idx) + 'U'
                 
+                # Finally, the ship object itself is updated.
+                # As is the set of occupied positions that is filled with the positions of the recently placed ship.
                 ship.place(ship_positions)
                 occupied_positions.update(ship_positions)
                 print(f"\n{self.name}'s ship, {ship.name} #{idx} has been successfully placed!")
@@ -161,20 +223,31 @@ class player:
         os.system("clear||cls")
 
     def play(self, *, graphical: bool = False):
+        """
+        Main gameplay loop corresponding to the battle phase.
+        Again, handles human, AI and graphical playing.
+        """
         target_valid = False
-        if self.isAI:
+        if self.isAI: # AI player branch
             while not target_valid:
+                # Last hit being None means no hit has happened yet or the last one was inexploitable.
                 if self.opponent.hits[-1] is None:
+                    # in this case, simply shoot at random.
                     xtar, ytar = random.randint(0, len(self.map[0])), random.randint(0, len(self.map))
                     target_valid = self.opponent.target_check(xtar, ytar, verbose=False)
+                # Use last hit to determine the next one
                 else:
                     yprev, xprev = self.opponent.hits[-1]
+                    # List 4 adjacent postions to last hit
                     possible_offsets = [(xprev - 1, yprev), (xprev + 1 , yprev), (xprev, yprev - 1), (xprev, yprev + 1)]
+                    # Eliminate invalid ones
                     valid_offsets = [(xoff, yoff) for xoff, yoff in possible_offsets if self.opponent.target_check(xoff, yoff, verbose=False)]
 
+                    # If no valid positions remain, then the last hit is useless, hence append None.
                     if valid_offsets == []:
                         self.opponent.hits.append(None)
                     else:
+                        # If the last two hits are in line, then assign highest priority to the move continuing the line (if valid)
                         if not self.opponent.hits[-2] is None:
                             yprev2, xprev2 = self.opponent.hits[-2]
                             ydir, xdir = yprev - yprev2, xprev - xprev2
@@ -184,16 +257,17 @@ class player:
                                 valid_offsets.remove(best_target)
                                 valid_offsets.insert(0, best_target)
                         
+                        # If a considered target is surrounded by hits, then assign lowest priority to it.
                         for i, (xoff, yoff) in enumerate(valid_offsets):
                             surroundings = {(xoff - 1, yoff), (xoff + 1 , yoff), (xoff, yoff - 1), (xoff, yoff + 1)}
 
                             if surroundings.issubset(self.opponent.hits):
                                 valid_offsets.append(valid_offsets.pop(i))
-                            
+                        
+                        # Finally, select first valid target because it has highest priority.
                         xtar, ytar = valid_offsets[0]
                         target_valid = True
-
-        else:
+        else: # Real player branch
             while not target_valid:
                 if graphical:
                     pass
@@ -221,16 +295,22 @@ class player:
         os.system("clear||cls")
 
     def get_shot(self, xtar: int, ytar: int):
-        assert self.map[ytar][xtar][-1] == 'U', f"Position ({ytar}, {xtar}) already hit."
+        """
+        Method which defines the action of being shot at by the other player.
+        """
+        assert self.map[ytar][xtar][-1] == 'U', f"Position ({ytar}, {xtar}) already hit." # Check that the target is unhit
 
+        # If target is water, then shot is a miss.
         if self.map[ytar][xtar][:-1] == 'W':
             self.map[ytar][xtar] = self.map[ytar][xtar][:-1] + 'M'
             print(f"\nX: {xtar}, Y: {ytar} // MISS!")
         else:
+            # Target is a boat, hence store id of the boat and type of hit.
             id = self.map[ytar][xtar][:-1]
             hit = self.ships[int(id)].damage(xtar, ytar)
             self.map[ytar][xtar] = id + hit
 
+            # If boat is sunk, mark all its cells as sunk and check if player should still be alive.
             if hit == 'S':
                 self.last_hit = None
                 for ypos, xpos in self.ships[int(id)].positions.keys():
@@ -243,12 +323,19 @@ class player:
                     if not self.ships[i].sunk:
                         self.alive = True
                     else: i += 1
+            # Boat is simply hit, nothing fancy happens.
             else:
                 self.hits.append((ytar, xtar))
                 print(f"\nX: {xtar}, Y: {ytar} // HIT!")
 
 class battle:
+    """
+    Class which represents an individual game of battleships and controls its flow.
+    """
     def __init__(self, gridsize: int, possible_ships: dict[str, list[tuple[bool, ...]]], P1_name: str, P2_name: str, P1isAI: bool = False, P2isAI: bool = False):
+        """
+        Simple constructor for battle object.
+        """
         self.gridzise = gridsize
 
         P1_ships = [ship(name, shape) for name, shape in possible_ships.items()]
@@ -260,23 +347,30 @@ class battle:
         self.player1.set_opponent(self.player2)
         self.player2.set_opponent(self.player1)
     
-    def start(self, *, graphical: bool = False):
+    def start(self, *, graphical: bool = False, **graphical_parameters):
+        """
+        Overarching gameplay loop of a battleships game, executes all portions of it from start to finish
+        """
+        self.player1.graphical_parameters = graphical_parameters
+        self.player2.graphical_parameters = graphical_parameters
+
         os.system("clear||cls")
         print("Let the game begin!\n")
 
-        # Ship placement procedure
+        # Ship placement procedure, both players set up their battlefied one after the other.
         self.player1.setup(graphical=graphical)
         self.player2.setup(graphical=graphical)
 
         # Start of actual game
+        # Loop stops when a winner is designated.
         winner = None
         while winner is None:
             self.player1.play(graphical=graphical)
-            if not self.player2.alive:
+            if not self.player2.alive: # If player 2 has died, then played 1 is the winner
                 winner = self.player1
-            else:
+            else: # Else player 2 can play its round
                 self.player2.play(graphical=graphical)
-                if not self.player1.alive:
+                if not self.player1.alive: # If player 1 has died, then played 2 is the winner
                     winner = self.player2
         
         print(f"\n{winner.name}, you won the game!")
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import game
 
-# Game setup
+# First, define game constants.
 gridsize = 10
 shapes = {"Carrier":    [(True,True,True,True,True)],
           "Battleship": [(True,True,True,True)],
@@ -9,29 +9,31 @@ shapes = {"Carrier":    [(True,True,True,True,True)],
           "Destroyer":  [(True,True)]
           }
 
+# Then, put default values for the parameters.
 P1_name, P2_name = "AI 1", "AI 2"
-P1isAI, P2isAI = "", ""
+P1isAI, P2isAI = None, None
 
-while isinstance(P1isAI, str):
-    P1isAI = input("Is player 1 an AI? (Yes/Y/y or No/N/n) ").lower()
-    if P1isAI[0] == "y":
+# Prompt the users to choose the parameters of the game.
+while P1isAI is None:
+    inp = input("Is player 1 an AI? (Yes/Y/y or No/N/n) ").lower()
+    if inp[0] == "y":
         P1isAI = True
-    elif P1isAI[0] == "n":
+    elif inp[0] == "n":
         P1isAI = False
         P1_name = input("What's player 1's name? ")
     else:
         print("\nInvalid inputs, please try again.")
 
-while isinstance(P2isAI, str):
+while P2isAI is None:
     P2isAI = input("Is player 2 an AI?  (Yes/Y/y or No/N/n) ").lower()
-    if P2isAI[0] == "y":
+    if inp[0] == "y":
         P2isAI = True
-    elif P2isAI[0] == "n":
+    elif inp[0] == "n":
         P2isAI = False
         P2_name = input("What's player 2's name? ")
     else:
         print("\nInvalid inputs, please try again.")
 
-
+# Finally, create the game with the given parameters and start it.
 bat = game.battle(gridsize, shapes, P1_name, P2_name, P1isAI, P2isAI)
 bat.start()
